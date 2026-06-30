48b1eaeae3e207c28f58be8c0740689a27a8dbe5
Maksim Martirosov <maksim@martirosoff.ru>
2026-06-30 20:12:47 +0300

    feat(setup-wizard): gate Продолжить on step validation (SP-3)

--- woodev/assets/build/setup-wizard/index.asset.php
+++ woodev/assets/build/setup-wizard/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8773a09ef2b08c11667b');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '275b4c448d5acb7bc360');
