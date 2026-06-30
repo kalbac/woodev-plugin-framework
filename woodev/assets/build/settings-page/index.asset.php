aab890d4d8b40e39dcc4b366eb6a2e8231296711
Maksim Martirosov <maksim@martirosoff.ru>
2026-06-30 19:38:23 +0300

    feat(ui-kit): FieldRow error display + abbr required marker (SP-3)

--- woodev/assets/build/settings-page/index.asset.php
+++ woodev/assets/build/settings-page/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '181ab647df248efb56c5');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '847da24e7fbb8ec5ac7f');
--- woodev/assets/build/setup-wizard/index.asset.php
+++ woodev/assets/build/setup-wizard/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0452e298f0e6d212672d');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '330f93ddf9b6dab67caf');
