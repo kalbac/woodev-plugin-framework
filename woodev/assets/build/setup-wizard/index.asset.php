b2a1ab5f8310e8586455116c40041bee9c1fb95e
Maksim Martirosov <maksim@martirosoff.ru>
2026-07-05 16:35:31 +0300

    feat(settings): hide conditional fields in the setup wizard + save gate

--- woodev/assets/build/setup-wizard/index.asset.php
+++ woodev/assets/build/setup-wizard/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '4041d460259f8957ed8a');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a61fa4958e9c56640e65');
