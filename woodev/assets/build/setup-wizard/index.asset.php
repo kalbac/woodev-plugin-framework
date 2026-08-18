39b09dab2603f5e26b1e34e426e1e261e38f14df
Maksim Martirosov <maksim@martirosoff.ru>
2026-08-19 02:37:30 +0300

    chore(build): rebuild JS bundles after merging main (missed in the merge commit)

--- woodev/assets/build/setup-wizard/index.asset.php
+++ woodev/assets/build/setup-wizard/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9942a04083037063d0c8');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '79db3448cbb04d16bbd2');
--- woodev/assets/build/ui-kit-gallery/index.asset.php
+++ woodev/assets/build/ui-kit-gallery/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8e1e72578c70d28e750e');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '88a3b30b2929dd383275');
