c859758e5cd843ac4287c59dc6a26d1422a70ad0
Maksim Martirosov <maksim@martirosoff.ru>
2026-08-21 03:22:07 +0300

    build(assets): refresh generated bundles

--- woodev/assets/build/setup-wizard/index.asset.php
+++ woodev/assets/build/setup-wizard/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e5e49873c88edbff91c3');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8b5668421c89bbfb5464');
--- woodev/assets/build/ui-kit-gallery/index.asset.php
+++ woodev/assets/build/ui-kit-gallery/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a8e01a6a012b5d96ed7a');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5be97878046dc3bb9966');
